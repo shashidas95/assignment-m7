e6105388e295edbe5a8b33216018e2630d14eba8
Shashi Kanta Das <60028859+shashidas95@users.noreply.github.com>
2025-04-18 15:52:03 +0600

    Update Numbers.php

--- Numbers.php
+++ Numbers.php
@@ -1,7 +1,7 @@
 <?php
 
 while (true) {
- print("Enter Some Numbers( Numbers Separated by space): ");
+ print("Enter Some Numbers( Numbers Separated by space and exit to quite): ");
  $inputs = trim(fgets(STDIN));
  //$inputs = readline();
 
@@ -11,33 +11,42 @@ while (true) {
  }
 
  $numbers = explode(" ", $inputs);
-
- $valid_numbers = [];
+ $all_valid = true;
+ $invalid_numbers = [];
 
  foreach ($numbers as $number) {
-  if (is_numeric($number)) {
-   $valid_numbers[] = $number;
-  } else {
-   echo "$number, is not a valid number \n";
+  if (!is_numeric($number)) {
+   $invalid_numbers[] = $number;
+   $all_valid = false;
   }
  }
- if (!empty($valid_numbers)) {
-  print_r($valid_numbers);
 
-  echo "Minimum:" . " " . min($valid_numbers) . "\n";
-  echo "Maximum:" . " " . max($valid_numbers) . "\n";
-
-  $count = count($valid_numbers);
-  $sum = 0;
-
-  foreach ($valid_numbers as $number) {
-   $sum += $number;
-  }
-
-  echo "Sum of the Numbers is :" . " " . $sum . "\n";
-  echo "Average of the Numbers is: " . " " . $sum / $count . "\n";
- } else {
-  echo "No valid number is available";
+ if (!empty($invalid_numbers)) {
+  echo "Please enter only numbers. \n";
+  echo "Invalid inputs: " . implode(", ", $invalid_numbers) . "\n";
  }
+ if (!$all_valid) {
+  continue; //ask for re-enter
+ }
+ $numbers = $numbers = array_map(function ($n) {
+  return floatval($n);
+ }, $numbers);
+ // $numbers = array_map('floatval', $numbers); //short form
+ print_r($numbers);
+ echo "Your valid inputs are " . implode(', ', $numbers) . "\n";
+
+ echo "Minimum:" . " " . min($numbers) . "\n";
+ echo "Maximum:" . " " . max($numbers) . "\n";
+
+ $count = count($numbers);
+ $sum = 0;
+
+ foreach ($numbers as $number) {
+  $sum += $number;
+ }
+
+ echo "Sum of the Numbers is :" . " " . $sum . "\n";
+ echo "Average of the Numbers is: " . " " . $sum / $count . "\n";
+
  echo "\n";
 }
